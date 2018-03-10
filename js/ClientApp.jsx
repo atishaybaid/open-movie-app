721a1594a7f0fff58f7c5b4d029c11df99be36f7
atishaybaid <atishaybaid@gmail.com>
2018-03-11 02:54:28 +0530

    hoc pattern form

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,36 +2,60 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-const ce = React.createElement;
-
-const MyTitle = function MyTitle(props) {
-  return React.createElement("div", null, React.createElement("h1", { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function MyFirstComponent() {
-  return React.createElement(
-    "div",
-    null,
-    React.createElement(
-      "h1",
-      null,
-      ce(MyTitle, { title: "My Second component", color: "YellowGreen" }),
-      ce(MyTitle, { title: "Game of throns", color: "GreenYellow" }),
-      ce(MyTitle, { title: "Silicon Valley", color: "peru" }),
-      ce(MyTitle, { title: "Band of Brothers", color: "LimeGreen" }),
-      ce(MyTitle)
-    )
+const LoginForm = props => {
+  return (
+    <div className="login-form">
+      <input
+        type="text"
+        placeholder="Your Name"
+        value={props.data.name}
+        onChange={props.onFieldChange.bind(this, "name")}
+      />
+      <input
+        type="text"
+        placeholder="Your email"
+        value={props.data.email}
+        onChange={props.onFieldChange.bind(this, "email")}
+      />
+      <input
+        type="text"
+        placeholder="Your password"
+        value={props.data.password}
+        onChange={props.onFieldChange.bind(this, "password")}
+      />
+      <input type="button" value="submit" onClick={props.onFormSubmit} />
+    </div>
   );
 };
 
-MyTitle.propTypes = {
-  title: PropTypes.string,
-  color: PropTypes.string
+const FormHOC = Form_Componet => {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: {}
+      };
+      this.onFieldChange = this.onFieldChange.bind(this);
+    }
+    onFieldChange(event, field) {
+      console.log(this);
+      console.log(field);
+      console.log(event.target);
+      /* var tempData = this.state.data;
+      console.log(tempData);
+      tempData[field] = event.target.value;
+      this.setState({ data: tempData });*/
+    }
+    onFormSubmit() {}
+
+    render() {
+      return (
+        <Form_Componet onFieldChange={this.onFieldChange} onFormSubmit={this.onFormSubmit} data={this.state.data} />
+      );
+    }
+  };
 };
 
-MyTitle.defaultProps = {
-  title: "My First component",
-  color: "peru"
-};
+const ConnectedForm = FormHOC(LoginForm);
 
-ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById("app"));
+ReactDOM.render(<ConnectedForm />, document.getElementById("app"));
